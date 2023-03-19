9ac1258b9225244fe4b2425ef1b1f16515c8dadc
Adil Hanney <adilhanney@disroot.org>
2023-03-19 02:41:06 +0000

    updates: add function to find direct download link

--- lib/components/settings/update_manager.dart
+++ lib/components/settings/update_manager.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 abstract class UpdateManager {
   static final Uri versionUrl = Uri.parse('https://raw.githubusercontent.com/adil192/saber/main/lib/data/version.dart');
+  static final Uri apiUrl = Uri.parse('https://api.github.com/repos/adil192/saber/releases/latest');
   /// The availability of an update.
   static final ValueNotifier<UpdateStatus> status = ValueNotifier(UpdateStatus.upToDate);
 
   static Future<void> showUpdateDialog(BuildContext context, {bool userTriggered = false}) async {
     // non-web only
+  }
+
+  @visibleForTesting
+  static Future<String?> getLatestDownloadUrl([
+    String? apiResponse,
+    TargetPlatform? platform,
+  ]) async {
+    // non-web only
+    return null;
   }
 }
 
@@ -15,6 +25,6 @@ enum UpdateStatus {
   upToDate,
   /// An update is available, but the user doesn't need to be notified
   updateOptional,
-  // An update is available and the user should be notified
+  /// An update is available and the user should be notified
   updateRecommended,
 }
